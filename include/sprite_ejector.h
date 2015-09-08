be91975faa3a5025b8593604e9485a894dfbdd80
brunonymous <bruno.ethvignot@gmail.com>
2015-09-08 17:17:28 +0200

    Adds the missing files.

---
+++ include/sprite_ejector.h
@@ -0,0 +1,51 @@
+/**
+ * @file sprite_ejector.h
+ * @brief Sprite ejector located in the corners
+ * @created 2015-04-12
+ * @date 2015-06-14
+ * @copyright 1991-2015 TLK Games
+ * @author Bruno Ethvignot
+ * @version $Revision: 24 $
+ */
+/*
+ * copyright (c) 1991-2015 TLK Games all rights reserved
+ * $Id: sprite_ejector.h 24 2014-09-28 15:30:04Z bruno.ethvignot@gmail.com $
+ *
+ * TecnoballZ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TecnoballZ is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+#ifndef __SPRITE_EJECTOR__
+#define __SPRITE_EJECTOR__
+class sprite_ejector;
+#include "../include/sprite_object.h"
+#include "../include/sprite_ball.h"
+#include "../include/controller_bricks.h"
+#include "../include/controller_balls.h"
+#include "../include/controller_ejectors.h"
+class sprite_ejector:public sprite_object
+{
+  friend class controller_balls;
+  friend class controller_ejectors;
+
+private:
+    Sint32 ball_offsetx;
+  Sint32 ball_offsety;
+
+public:
+    sprite_ejector (Sint32 ball_offx, Sint32 ball_offy);
+   ~sprite_ejector ();
+  void stick (sprite_ball * ball);
+};
+#endif
---
+++ src/sprite_ejector.cc
@@ -0,0 +1,61 @@
+/**
+ * @file sprite_ejector.cc
+ * @brief Sprite ejector located in the corners
+ * @created 2015-04-10
+ * @date 2015-05-14
+ * @copyright 1991-2015 TLK Games
+ * @author Bruno Ethvignot
+ * @version $Revision$
+ */
+/*
+ * copyright (c) 1991-2015 TLK Games all rights reserved
+ * $Id: sprite_ejector.cc 24 2014-09-28 15:30:04Z bruno.ethvignot@gmail.com $
+ *
+ * TecnoballZ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TecnoballZ is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+#include "../include/sprite_ejector.h"
+
+/**
+ * Create the object
+ */
+sprite_ejector::sprite_ejector (Sint32 ball_offx, Sint32 ball_offy)
+{
+  ball_offsetx = ball_offx * resolution;
+  ball_offsety = ball_offy * resolution;
+  if (resolution == 2)
+    {
+      ball_offsetx++;
+      ball_offsety++;
+    }
+  clear_sprite_members ();
+}
+
+/**
+ * Release the object
+ */
+sprite_ejector::~sprite_ejector ()
+{
+}
+
+/**
+ * Places the ball on the ejector
+ * @param ball pointer to sprite_ball object
+ */
+void
+sprite_ejector::stick (sprite_ball * ball)
+{
+  ball->pull (this, ball_offsetx, ball_offsety);
+}
